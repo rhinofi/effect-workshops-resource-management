2cb45e21108b84bb08f9b9c7276d72907fc89bf0
Adrian Gierakowski <agierakowski@gmail.com>
2024-11-15 11:21:05 +0000

    fix

--- src/resource-management/access-and-provide-multiple-services-gen-with-context.ts
+++ src/resource-management/access-and-provide-multiple-services-gen-with-context.ts
@@ -5,7 +5,7 @@ import { B } from './B.js'
 const program = Effect.gen(function*() {
   const a = yield* A
   yield* Console.log('service A instance:', a)
-  const b = yield* A
+  const b = yield* B
   yield* Console.log('service B instance:', b)
 })
 
